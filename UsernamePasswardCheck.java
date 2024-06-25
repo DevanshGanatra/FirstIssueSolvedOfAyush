c71e41a40000da758ef3824ad53f22096e5461fe
ajscodes <ayushmaradia@gmail.com>
2024-06-25 20:49:19 +0530

    Updated

--- UsernamePasswardCheck.java
+++ UsernamePasswardCheck.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class UsernamePasswardCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String correctUsername = "abc_123";
-        String correctPassward = "abc.z5152";
+        String correctUsername = "User_5632";
+        String correctPassward = "user.8976#32";
         System.out.print("Enter your username: ");
         String enteredUsername = sc.nextLine();
         System.out.print("Enter your passward: ");
